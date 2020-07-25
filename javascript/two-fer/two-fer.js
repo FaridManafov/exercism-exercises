e36131f6854798393dc8472e003565cbc4920fc0
Farid Manafov <faridbardium@gmail.com>
2020-07-25 18:24:48 -0400

    finished two-fer exercise

--- javascript/two-fer/two-fer.js
+++ javascript/two-fer/two-fer.js
@@ -3,6 +3,15 @@
 // convenience to get you started writing code faster.
 //
 
-export const twoFer = () => {
-  throw new Error("Remove this statement and implement this function");
+export const twoFer = (name) => {
+  let string = `One for ${name ? name : "you"}, one for me.`
+  
+  if (name === undefined) {
+    return string
+  }
+  
+  else {
+    return string
+  }
+  
 };
