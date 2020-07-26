3b1550088d046d0c51e742fae684cfd6f2a6511c
Farid Manafov <faridbardium@gmail.com>
2020-07-26 16:54:04 -0400

    incompleted challenge, stuck on undefined bug on else statement

--- javascript/resistor-color/resistor-color.js
+++ javascript/resistor-color/resistor-color.js
@@ -3,8 +3,25 @@
 // convenience to get you started writing code faster.
 //
 
-export const colorCode = () => {
-  throw new Error("Remove this statement and implement this function");
+export const colorCode = (i) => {
+  const resistorColors = ["black", "brown", "red", "orange", "yellow", "green", "blue", "violet", "grey", "white"]
+  let userInput = i.toLowerCase()
+  let result
+
+  for (let i = 0; i < resistorColors.length; i++) {
+    const colorCodeOutput = resistorColors[i];
+    
+    if (colorCodeOutput === userInput) {
+      result = i
+    }
+
+    else{
+      result = resistorColors
+    }
+
+  }
+
+  return result
 };
 
 export const COLORS = undefined;
